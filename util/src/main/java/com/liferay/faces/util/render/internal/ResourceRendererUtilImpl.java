a3d8bd31cae46a5325842909a2a6fcc71bd1650e
Neil Griffin <neil.griffin.scm@gmail.com>
2016-02-10 16:03:23 -0500

    FACES-2544 Change API of the ResourceDependencyVerifier.isResourceDependencySatisfied(UIComponent) to ResourceVerifier.isDependencySatisfied(FacesContext, UIComponent) (Change the implementation of removal of satisfied dependencies to remove duplicate code from the bridge. Also fix ResourceVerifierFactory.getResourceVerifierInstance() method name.)

--- util/src/main/java/com/liferay/faces/util/render/internal/ResourceRendererUtilImpl.java
+++ util/src/main/java/com/liferay/faces/util/render/internal/ResourceRendererUtilImpl.java
@@ -28,12 +28,12 @@ import javax.faces.event.ComponentSystemEventListener;
 import javax.faces.event.ListenerFor;
 import javax.faces.event.PostAddToViewEvent;
 import javax.faces.render.Renderer;
-import javax.faces.render.RendererWrapper;
 
 import com.liferay.faces.util.application.ResourceVerifier;
 import com.liferay.faces.util.application.ResourceVerifierFactory;
 import com.liferay.faces.util.logging.Logger;
 import com.liferay.faces.util.logging.LoggerFactory;
+import com.liferay.faces.util.render.RendererWrapper;
 
 
 /**
