ee529ccc67a0afcfeb43e68badcab97b347c6d6c
Kyle Stiemann <stiemannkj1@gmail.com>
2016-07-27 17:39:23 -0400

    Add comment explaining thread-safety of FactoryExtensionFinder's lazy initialization. Make FactoryExtensionFinder's instance loading code more consistent with 2.x and 3.x branches to make backports easier.

--- src/main/java/com/liferay/faces/util/factory/FactoryExtensionFinder.java
+++ src/main/java/com/liferay/faces/util/factory/FactoryExtensionFinder.java
@@ -19,6 +19,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.faces.FacesException;
 
@@ -39,68 +44,123 @@ public abstract class FactoryExtensionFinder {
 
 	public static FactoryExtensionFinder getInstance() throws FacesException {
 
+		// This method of lazy-initialization is thread-safe because the FactoryExtensionFinder is first called during
+		// ApplicationStartupListener.processSystemEvent(). ApplicationStartupListener.processSystemEvent() occurs
+		// before multiple threads have the opportunity to concurrently access the FactoryExtensionFinder.
 		if (instance == null) {
 
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			ServiceFinder<FactoryExtensionFinder> serviceLoader = ServiceFinder.load(FactoryExtensionFinder.class);
 
-			try {
-				String facesFactoryFinderService =
-					"META-INF/services/com.liferay.faces.util.factory.FactoryExtensionFinder";
-				String facesFactoryFinderClassName = getClassPathResourceAsString(facesFactoryFinderService);
+			if (serviceLoader != null) {
 
-				if (facesFactoryFinderClassName != null) {
-					Class<?> facesFactoryFinderClass = classLoader.loadClass(facesFactoryFinderClassName);
-					instance = (FactoryExtensionFinder) facesFactoryFinderClass.newInstance();
+				Iterator<FactoryExtensionFinder> iterator = serviceLoader.iterator();
+
+				while ((instance == null) && iterator.hasNext()) {
+					instance = iterator.next();
 				}
-				else {
-					throw new FacesException("Unable to load resource=[" + facesFactoryFinderService + "]");
+
+				if (instance == null) {
+					throw new FacesException("Unable locate service for " + FactoryExtensionFinder.class.getName());
 				}
 			}
-			catch (Exception e) {
-				throw new FacesException(e);
+			else {
+				throw new FacesException("Unable to acquire ServiceLoader for " +
+					FactoryExtensionFinder.class.getName());
 			}
 		}
 
 		return instance;
 	}
 
-	private static String getClassPathResourceAsString(String resourcePath) {
-		String classPathResourceAsString = null;
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	public abstract Object getFactoryInstance(Class<?> clazz);
 
-		if (classLoader != null) {
-			InputStream inputStream = classLoader.getResourceAsStream(resourcePath);
+	public abstract void registerFactory(ConfiguredElement configuredFactoryExtension);
 
-			if (inputStream != null) {
-				InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-				BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+	private static final class ServiceFinder<S> implements Iterable<S> {
+
+		private Class<S> serviceClass;
+
+		private ServiceFinder(Class<S> serviceClass) {
+			this.serviceClass = serviceClass;
+		}
+
+		private static <S> ServiceFinder<S> load(Class<S> serviceClass) {
+			return new ServiceFinder(serviceClass);
+		}
+
+		public Iterator<S> iterator() {
+
+			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+			List<S> instances = new ArrayList<S>();
+			Enumeration<URL> resources = null;
+
+			try {
+				resources = classLoader.getResources("META-INF/services/" + serviceClass.getName());
+			}
+			catch (IOException e) {
+
+				System.err.println("Unable to obtain resources via path=[META-INF/services/" + serviceClass.getName() +
+					"]:");
+				System.err.println(e);
+			}
+
+			while ((resources != null) && resources.hasMoreElements()) {
+
+				URL resource = resources.nextElement();
+				InputStream inputStream = null;
+				InputStreamReader inputStreamReader = null;
+				BufferedReader bufferedReader = null;
+				String className = null;
 
 				try {
-					classPathResourceAsString = bufferedReader.readLine();
+
+					inputStream = resource.openStream();
+					inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+					bufferedReader = new BufferedReader(inputStreamReader);
+					className = bufferedReader.readLine();
+
 				}
 				catch (IOException e) {
-
-					// Since the API can't use a logging system like SLF4J the best we can do is print to stderr.
-					System.err.println("Unable to read contents of resourcePath=[" + resourcePath + "]");
+					System.err.println("Unable to read contents of resource=[" + resource.getPath() + "]");
 				}
 				finally {
 
 					try {
-						bufferedReader.close();
-						inputStreamReader.close();
-						inputStream.close();
+
+						if (bufferedReader != null) {
+							bufferedReader.close();
+						}
+
+						if (inputStreamReader != null) {
+							inputStreamReader.close();
+						}
+
+						if (inputStream != null) {
+							inputStream.close();
+						}
 					}
 					catch (IOException e) {
-						e.printStackTrace();
+						// ignore
+					}
+				}
+
+				if (className != null) {
+
+					try {
+
+						Class<?> clazz = Class.forName(className);
+						S instance = (S) clazz.newInstance();
+						instances.add(instance);
+					}
+					catch (Exception e) {
+
+						System.err.println("Unable to instantiate class=[" + className + "]:");
+						System.err.println(e);
 					}
 				}
 			}
+
+			return instances.iterator();
 		}
-
-		return classPathResourceAsString;
 	}
-
-	public abstract Object getFactoryInstance(Class<?> clazz);
-
-	public abstract void registerFactory(ConfiguredElement configuredFactoryExtension);
 }
