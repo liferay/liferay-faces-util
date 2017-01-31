f0191cccc42303dc8842621261fd5f64fb3fc882
Juan G <juan.gonzalez@liferay.com>
2017-01-31 10:37:29 +0100

    FACES-2996 Add resource bundle control to force loading in UTF-8

---
+++ src/main/java/com/liferay/faces/util/i18n/UTF8Control.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.util.i18n;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.Locale;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+import java.util.ResourceBundle.Control;
+
+
+/** Control for getting resource bundles with UTF8 encoding. Based on default Control impl.
+ * @author  Juan Gonzalez
+ */
+public class UTF8Control extends Control {
+	public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+		throws IllegalAccessException, InstantiationException, IOException {
+
+		String bundleName = toBundleName(baseName, locale);
+		ResourceBundle bundle = null;
+
+		if (format.equals("java.class")) {
+
+			try {
+				Class<? extends ResourceBundle> bundleClass = (Class<? extends ResourceBundle>) loader.loadClass(
+						bundleName);
+
+				// If the class isn't a ResourceBundle subclass, throw a
+				// ClassCastException.
+				if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+					bundle = bundleClass.newInstance();
+				}
+				else {
+					throw new ClassCastException(bundleClass.getName() + " cannot be cast to ResourceBundle");
+				}
+			}
+			catch (ClassNotFoundException e) {
+			}
+		}
+		else if (format.equals("java.properties")) {
+			final String resourceName = toResourceName(bundleName, "properties");
+			final ClassLoader classLoader = loader;
+			final boolean reloadFlag = reload;
+			InputStream stream = null;
+
+			try {
+				stream = AccessController.doPrivileged(new PrivilegedExceptionAction<InputStream>() {
+							public InputStream run() throws IOException {
+								InputStream is = null;
+
+								if (reloadFlag) {
+									URL url = classLoader.getResource(resourceName);
+
+									if (url != null) {
+										URLConnection connection = url.openConnection();
+
+										if (connection != null) {
+
+											// Disable caches to get fresh data for
+											// reloading.
+											connection.setUseCaches(false);
+											is = connection.getInputStream();
+										}
+									}
+								}
+								else {
+									is = classLoader.getResourceAsStream(resourceName);
+								}
+
+								return is;
+							}
+						});
+			}
+			catch (PrivilegedActionException e) {
+				throw (IOException) e.getException();
+			}
+
+			if (stream != null) {
+
+				try {
+					bundle = new PropertyResourceBundle(new InputStreamReader(stream, "UTF-8"));
+				}
+				finally {
+					stream.close();
+				}
+			}
+		}
+		else {
+			throw new IllegalArgumentException("unknown format: " + format);
+		}
+
+		return bundle;
+	}
+}
