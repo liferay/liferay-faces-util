c5c5fb9207da4425f3fa39fd670af5edd5993c94
Neil Griffin <neil.griffin.scm@gmail.com>
2017-08-22 17:47:10 -0400

    FACES-3187 Product.SERVLET_CONTAINER returns "Jetty 0.0.0" when running in Liferay Portal 7.0

---
+++ src/main/java/com/liferay/faces/util/product/internal/ProductLiferayWabExtenderImpl.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.util.product.internal;
+
+/**
+ * @author  Neil Griffin
+ */
+public class ProductLiferayWabExtenderImpl extends ProductBaseImpl {
+
+	public ProductLiferayWabExtenderImpl(int buildId, int majorVersion, int minorVersion, int patchVersion,
+		String version, boolean detected) {
+		this.buildId = buildId;
+		this.majorVersion = majorVersion;
+		this.minorVersion = minorVersion;
+		this.patchVersion = patchVersion;
+		this.version = version;
+		this.detected = detected;
+		this.title = "Liferay WAB Extender";
+	}
+}
