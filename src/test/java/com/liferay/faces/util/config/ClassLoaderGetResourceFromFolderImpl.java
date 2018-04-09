3d7a92bd21eea0f1421b91e4f0dd26b1bc0f76e7
Kyle Stiemann <stiemannkj1@gmail.com>
2018-04-09 15:53:12 -0400

    FACES-3265 WebConfigParserImpl doesn't obtain context params

---
+++ src/test/java/com/liferay/faces/util/config/ClassLoaderGetResourceFromFolderImpl.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.util.config;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class ClassLoaderGetResourceFromFolderImpl extends ClassLoader {
+
+	// Private Data Members
+	private final String[] resourceFolders;
+
+	public ClassLoaderGetResourceFromFolderImpl(ClassLoader classLoader, String... resourceFolders) {
+
+		super(classLoader);
+		this.resourceFolders = resourceFolders;
+	}
+
+	private static String getResourceName(final String RESOURCE_FOLDER_PREFIX, String resourceName) {
+
+		if (!resourceName.startsWith(RESOURCE_FOLDER_PREFIX)) {
+
+			if (!resourceName.startsWith("/")) {
+				resourceName = "/" + resourceName;
+			}
+
+			resourceName = RESOURCE_FOLDER_PREFIX + resourceName;
+		}
+
+		return resourceName;
+	}
+
+	@Override
+	public URL getResource(String name) {
+
+		URL resource = null;
+
+		for (String resourceFolder : resourceFolders) {
+
+			resource = super.getResource(getResourceName(resourceFolder, name));
+
+			if (resource != null) {
+				break;
+			}
+		}
+
+		return resource;
+	}
+
+	@Override
+	public InputStream getResourceAsStream(String name) {
+
+		InputStream resourceAsStream = null;
+
+		for (String resourceFolder : resourceFolders) {
+
+			resourceAsStream = super.getResourceAsStream(getResourceName(resourceFolder, name));
+
+			if (resourceAsStream != null) {
+				break;
+			}
+		}
+
+		return resourceAsStream;
+	}
+
+	@Override
+	public Enumeration<URL> getResources(String name) throws IOException {
+
+		List<URL> resources = new ArrayList<URL>();
+
+		for (String resourceFolder : resourceFolders) {
+			resources.addAll(Collections.list(super.getResources(getResourceName(resourceFolder, name))));
+		}
+
+		return Collections.enumeration(resources);
+	}
+}
---
+++ src/test/java/com/liferay/faces/util/config/WebConfigScannerTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.liferay.faces.util.config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.junit.Test;
+
+import org.xml.sax.SAXException;
+
+import com.liferay.faces.util.config.internal.WebConfigScanner;
+import com.liferay.faces.util.config.internal.WebConfigScannerImpl;
+import com.liferay.faces.util.xml.ConcurrentSAXParserFactory;
+
+import junit.framework.Assert;
+
+
+/**
+ * @author  Kyle Stiemann
+ */
+public class WebConfigScannerTest {
+
+	@Test
+	public void testWebConfigScanner() throws ParserConfigurationException, SAXException, IOException {
+
+		SAXParserFactory saxParserFactory = ConcurrentSAXParserFactory.newInstance();
+		saxParserFactory.setValidating(false);
+		saxParserFactory.setNamespaceAware(true);
+
+		// Obtain a SAX Parser from the factory.
+		SAXParser saxParser = saxParserFactory.newSAXParser();
+
+		// Scan all the web.xml and web-fragment.xml descriptors in the classpath.
+		final ClassLoader CLASS_LOADER = new ClassLoaderGetResourceFromFolderImpl(Thread.currentThread()
+				.getContextClassLoader(), "web-config-scanner-test", "web-config-scanner-test/0",
+				"web-config-scanner-test/1");
+		WebConfigScanner webConfigScanner = new WebConfigScannerImpl(CLASS_LOADER,
+				new ResourceReaderTestImpl(CLASS_LOADER), saxParser, false);
+		WebConfig webConfig = webConfigScanner.scan();
+
+		List<ConfiguredServlet> configuredServlets = webConfig.getConfiguredServlets();
+		Assert.assertEquals(1, configuredServlets.size());
+
+		ConfiguredServlet configuredServlet = configuredServlets.get(0);
+		Assert.assertEquals("Faces Servlet", configuredServlet.getServletName());
+		Assert.assertEquals("javax.faces.webapp.FacesServlet", configuredServlet.getServletClass());
+		Assert.assertNull(configuredServlet.getMultiPartConfig());
+
+		List<ConfiguredServletMapping> configuredServletMappings = webConfig.getConfiguredServletMappings();
+		Assert.assertEquals(1, configuredServletMappings.size());
+
+		ConfiguredServletMapping configuredServletMapping = configuredServletMappings.get(0);
+		Assert.assertEquals("Faces Servlet", configuredServletMapping.getServletName());
+		Assert.assertEquals("*.xhtml", configuredServletMapping.getUrlPattern());
+		Assert.assertEquals(".xhtml", configuredServletMapping.getExtension());
+		Assert.assertTrue(configuredServletMapping.isExtensionMapped());
+		Assert.assertFalse(configuredServletMapping.isImplicit());
+		Assert.assertFalse(configuredServletMapping.isPathMapped());
+		Assert.assertFalse(configuredServletMapping.isPathMapped());
+		Assert.assertTrue(configuredServletMapping.isMatch("asdf.xhtml"));
+		Assert.assertFalse(configuredServletMapping.isMatch("xhtml/asdf"));
+		Assert.assertNull(configuredServletMapping.getServletPath());
+
+		// FACES-3265 WebConfigParserImpl doesn't obtain context params
+		Map<String, String> configuredContextParams = webConfig.getConfiguredContextParams();
+		Assert.assertEquals("com.liferay.faces.alloy.tagdecorator.internal.TagDecoratorAlloyImpl",
+			configuredContextParams.get("javax.faces.FACELETS_DECORATORS"));
+		Assert.assertEquals("true",
+			configuredContextParams.get("javax.faces.ALWAYS_PERFORM_VALIDATION_WHEN_REQUIRED_IS_TRUE"));
+		Assert.assertEquals("Production", configuredContextParams.get("javax.faces.PROJECT_STAGE"));
+		Assert.assertEquals("/WEB-INF/resources",
+			configuredContextParams.get("javax.faces.WEBAPP_RESOURCES_DIRECTORY"));
+		Assert.assertNull(configuredContextParams.get("asdf"));
+	}
+}
