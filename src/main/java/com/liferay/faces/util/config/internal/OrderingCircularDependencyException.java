9c4c618403f842dbd6aaa759091d86cf3f28e602
Kyle Stiemann <stiemannkj1@gmail.com>
2018-04-25 11:15:29 -0400

    Throwables must be Serializable

--- src/main/java/com/liferay/faces/util/config/internal/OrderingCircularDependencyException.java
+++ src/main/java/com/liferay/faces/util/config/internal/OrderingCircularDependencyException.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2000-2017 Liferay, Inc. All rights reserved.
+ * Copyright (c) 2000-2018 Liferay, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
  */
 package com.liferay.faces.util.config.internal;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
@@ -23,7 +24,10 @@ import java.util.List;
 /**
  * @author  Vernon Singleton
  */
-public class OrderingCircularDependencyException extends Exception {
+public class OrderingCircularDependencyException extends Exception implements Serializable {
+
+	// serialVersionUID
+	private static final long serialVersionUID = 6848057442511766527L;
 
 	public OrderingCircularDependencyException(Ordering.Path path, List<FacesConfigDescriptor> facesConfigs) {
 		super(createMessage(path, facesConfigs));
