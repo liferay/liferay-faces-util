d545ae08e4bb2a807017cf8c5e6877058ee2a1c3
Neil Griffin <neil.griffin.scm@gmail.com>
2020-07-20 15:07:07 -0400

    FACES-3295 Add @ProviderType and @ConsumerType annotations to public API (increase minor version from 3.3.1-SNAPSHOT to 3.4.0-SNAPSHOT in order to reflect the significance of thin-WAR support via FACES-3024)

--- src/main/java/com/liferay/faces/util/xml/package-info.java
+++ src/main/java/com/liferay/faces/util/xml/package-info.java
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@org.osgi.annotation.versioning.Version("3.3.1")
+@org.osgi.annotation.versioning.Version("3.4.0")
 package com.liferay.faces.util.xml;
