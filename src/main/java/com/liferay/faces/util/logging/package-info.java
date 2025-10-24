2728c30b6b2e5b5ab1dbe2371a61efc34b324d39
Neil Griffin <neil.griffin.scm@gmail.com>
2025-10-23 20:14:00 -0400

    Fix versions

--- src/main/java/com/liferay/faces/util/logging/package-info.java
+++ src/main/java/com/liferay/faces/util/logging/package-info.java
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@org.osgi.annotation.versioning.Version("3.4.1")
+@org.osgi.annotation.versioning.Version("3.4.2")
 package com.liferay.faces.util.logging;
